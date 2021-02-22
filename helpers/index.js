0ea249c34d09649a12a7def717ae2eba9577b5e1
view4 <morrisgaby987@googlemail.com>
2021-02-22 15:26:00 +0000

    enable serving the client code from server

--- helpers/index.js
+++ helpers/index.js
@@ -15,6 +15,12 @@ helpers.getRoute = (pathname, method) => {
     } else if (pathname.includes("/steps")){
         route = router.steps[method];
         belief = helpers.parseBelief(pathname)
+    } else if (pathname ==="/"){
+        route= router.client.html;
+    } else if (pathname.includes("/client")){
+        let paths = pathname.split("/")
+        let path = paths[paths.length-1]
+        route = router.client[path]
     }
 
     return {route, belief }
--- router.js
+++ router.js
@@ -1,8 +1,10 @@
+const { readFile } = require("fs");
 const document = require("./helpers/document");
 const writer = require ("./helpers/writer");
 const router = {};
 router.kadesh = {};
 router.steps = {};
+router.client = {};
 
 router.kadesh.get = (req, res) => {
     document.read("kadesh", "index", (data) => {
@@ -27,6 +29,39 @@ router.steps.post = (req, res) => {
     writer.steps(steps)
     return res.end("there is great belief")
 };
+
+router.client.html = (req, res) => {
+    const mimeType = "text/html";
+    const filePath = "./client/steps.html"
+    readFile(filePath, (err, content) => {
+        if(!err){
+            res.writeHead(200, {"Content-Type": mimeType})
+            res.end(content, "utf-8")
+        }
+    })
+}
+
+router.client.css = (req, res) => {
+    const mimeType = "text/css";
+    const filePath = "./client/css/steps.css"
+    readFile(filePath, (err, content) => {
+        if(!err){
+            res.writeHead(200, {"Content-Type": mimeType})
+            res.end(content, "utf-8")
+        }
+    })
+}
+
+router.client.faith = (req, res) => {
+    const mimeType = "text/javascript";
+    const filePath = "./client/js/hope_and_faith.js"
+    readFile(filePath, (err, content) => {
+        if(!err){
+            res.writeHead(200, {"Content-Type": mimeType})
+            res.end(content, "utf-8")
+        }
+    })
+}
 
 
 module.exports = router
